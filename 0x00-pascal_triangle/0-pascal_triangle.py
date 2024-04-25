ce950c63eadc4af96fbd862ae9b9983a87a9685e
aminoxlhd <aminoxlhd@gmail.com>
2024-04-25 23:44:54 +0000

    add 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+def pascal_triangle(n):
+  """
+   A function that returns a list of lists of integers representing the Pascal s triangle of n
+  Args:
+      n: The number of rows in the Pascal's triangle.
+  Returns:
+      A list of lists representing the Pascal's triangle.
+  """
+  triangle = []
+
+  if n == 0:
+    return triangle
+  elif n == 1:
+    triangle.append([1])
+    return triangle
+
+  for index in range(n):
+    row = []
+    row.append(1)
+
+    if index > 1:
+      for index2 in range(1, index):
+        prev_row = triangle[index - 1]
+        element = prev_row[index2 - 1] + prev_row[index2]
+        row.append(element)
+
+    row.append(1)
+    triangle.append(row)
+
+  return triangle
