32b4ead8df03a1bc9a21113ba3cae45883798b95
aminoxlhd <aminoxlhd@gmail.com>
2024-04-28 16:55:46 +0000

    add 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,16 +15,21 @@ def pascal_triangle(n):
     for index in range(n):
         row = []
         row.append(1)
-
-        if index > 1:
-            index2 = 1
-            while index2 < index:
+        index2 = 1
+        while index2 <= index:
+            if index == 0:
+                prev_row = [1]
+            else:
                 prev_row = triangle[index - 1]
-                element = prev_row[index2 - 1] + prev_row[index2]
-                row.append(element)
-                index2 = index2 + 1
 
-        row.append(1)
+                if index2 < index:
+                    element = prev_row[index2 - 1] + prev_row[index2]
+                else:
+                    element = prev_row[index2 - 1]
+
+                row.append(element)
+                index2 += 1
+
         triangle.append(row)
 
     return triangle
