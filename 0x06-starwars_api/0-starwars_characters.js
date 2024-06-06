af09edf245677c2754cec71a891a5b4622d60bff
aminoxlhd <aminoxlhd@gmail.com>
2024-06-06 21:24:58 +0100

    add task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+const makeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
+
+const fetchCharacters = async () => {
+  try {
+    const movieData = await makeRequest(apiUrl);
+    const characterUrls = movieData.characters;
+
+    for (const url of characterUrls) {
+      const characterData = await makeRequest(url);
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+fetchCharacters();
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+README for Star Wars API
