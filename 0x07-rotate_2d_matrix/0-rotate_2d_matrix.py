f604c00e44c1baf4c9488e5c09c7bf5a3a9c4eff
aminoxlhd <aminoxlhd@gmail.com>
2024-06-17 01:14:38 +0100

    edit 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,7 +2,7 @@
 """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
 
 
-def rotate_2d_matrix(matrix):
+def rotate_2d_matrix(input_matrix):
     """Rotates a 2D matrix in-place by 90 degrees clockwise.
 
         Args:
@@ -11,15 +11,16 @@ def rotate_2d_matrix(matrix):
         Returns:
             None. The matrix is rotated in-place.
         """
-    num_rows = len(matrix)
-
-    for layer in range(num_rows // 2):
-        first_index = layer
-        last_index = num_rows - 1 - layer
-        for i in range(first_index, last_index):
-            temp = matrix[first_index][i]
-            matrix[first_index][i] = matrix[last_index - i][first_index]
-            matrix[last_index - i][first_index] = matrix[last_index][
-                last_index - i]
-            matrix[last_index][last_index - i] = matrix[i][last_index]
-            matrix[i][last_index] = temp
+    num_rows = len(input_matrix)
+    for start_row in range(num_rows // 2):
+        for col_index in range(start_row, num_rows - start_row - 1):
+            temp_value = input_matrix[start_row][col_index]
+            input_matrix[start_row][col_index] = input_matrix[
+                num_rows - 1 - col_index][start_row]
+            input_matrix[num_rows - 1 - col_index][start_row] = input_matrix[
+                num_rows - 1 - start_row][
+                num_rows - 1 - col_index]
+            input_matrix[num_rows - 1 - start_row][
+                num_rows - 1 - col_index] = input_matrix[col_index][
+                num_rows - 1 - start_row]
+            input_matrix[col_index][num_rows - 1 - start_row] = temp_value
