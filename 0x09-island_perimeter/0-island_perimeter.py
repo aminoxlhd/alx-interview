58ee9f07d34c3836aea1f13c3390ad09b3434768
aminoxlhd <aminoxlhd@gmail.com>
2024-06-27 22:01:07 +0100

    add 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """
+      a function def island_perimeter(grid): that returns the perimeter
+      of the island described in grid
+
+      Args:
+          grid: a list of integers representing the grid.
+
+      Returns:
+          The perimeter of the island described in grid
+      """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
+                    perimeter += 1
+                    continue
+
+                if grid[row - 1][col] == 0:
+                    perimeter += 1
+                if grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col > 0 and grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col < cols - 1 and grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
