d6439dbc9bde545c4b295778b229ffd29e0a4a5e
aminoxlhd <aminoxlhd@gmail.com>
2024-07-05 03:27:12 +0100

    edit 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,41 +4,56 @@ Prime Game
 """
 
 
-def findPrimes(n):
-    """
-    This function finds all prime numbers between 1 and n
-    """
-    primes = []
-    if n >= 2:
-        primes.append(2)
-    for num in range(3, n + 1, 2):
-        is_prime = True
-        for i in range(3, int(num ** 0.5) + 1, 2):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(num)
+def sieve_of_eratosthenes(n):
+    """sieve_of_eratosthenes function"""
+    is_prime = [True] * (n + 1)
+    p = 2
+    while p * p <= n:
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    primes = [p for p in range(2, n + 1) if is_prime[p]]
     return primes
 
 
+def count_prime_moves(n, primes):
+    """count_prime_moves function"""
+    available = [True] * (n + 1)
+    moves = 0
+
+    for prime in primes:
+        if prime > n:
+            break
+        if available[prime]:
+            moves += 1
+            for multiple in range(prime, n + 1, prime):
+                available[multiple] = False
+
+    return moves
+
+
 def isWinner(x, nums):
-    """
-    determine who the winner of a game
-    """
-    if x is None or nums is None or x == 0 or nums == []:
+    """isWinner function"""
+    if x <= 0 or not nums:
         return None
-    countA = 0
-    countB = 0
-    for n in range(x):
-        primes = findPrimes(nums[n])
-        if len(primes) % 2 != 0:
-            countA = countA + 1
+
+    max_n = max(nums)
+    primes = sieve_of_eratosthenes(max_n)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        moves = count_prime_moves(n, primes)
+        if moves % 2 == 1:
+            maria_wins += 1
         else:
-            countB = countB + 1
-    if countA == countB:
-        return None
-    if countA > countB:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    else:
+    elif ben_wins > maria_wins:
         return "Ben"
+    else:
+        return None
